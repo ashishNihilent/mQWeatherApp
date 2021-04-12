6aa60815a71b4e06019ff54596eff65b37471a56
Ashish Kadam <kaddam.ashishb@gmail.com>
2021-04-12 20:20:24 +0530

    Commit 10

--- app/src/main/java/com/kadamab/weather/rm/RealmApplication.kt
+++ app/src/main/java/com/kadamab/weather/rm/RealmApplication.kt
@@ -8,8 +8,11 @@ class RealmApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        Realm.init(this)
-        val config = RealmConfiguration.Builder().build()
-        Realm.setDefaultConfiguration(config)
+        val realmConfiguration = RealmConfiguration.Builder(this)
+            .name(Realm.DEFAULT_REALM_NAME)
+            .schemaVersion(0)
+            .deleteRealmIfMigrationNeeded()
+            .build()
+        Realm.setDefaultConfiguration(realmConfiguration)
     }
 }
